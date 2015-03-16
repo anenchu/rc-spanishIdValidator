3459438b37f9a61db904b048a080a6672989d661
AngelaPrades <angelaprades@gmail.com>
2015-03-16 18:32:47 +0100

    commit initial gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -8,33 +8,15 @@ module.exports = function(grunt) {
             port: 8080,
             keepalive:true,
             open: {
-              target: 'http://localhost:8080/src/index.html'
+              target: 'http://localhost:8080/src'
             }            
           },
         }
      },
-      uglify: {
-        options: {
-          mangle: false
-        },
-        files: { 
-               src: 'src/*/*.js',  // source files mask
-               dest: 'build/',    // destination folder
-               expand: true,    // allow dynamic building
-               flatten: true,   // remove all unnecessary nesting
-           },
-      },
   });
 
-  grunt.registerTask('default', ['web','uglify'], function (){
-    grunt.task.run('uglify');
-  });
+  grunt.registerTask('default', ['web']);
   
-  //grunt.registerTask('web', ['connect']);
   grunt.registerTask('web', ['connect:server']);
-  grunt.registerTask('uglify', ['connect:server']);
-
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   
 };
